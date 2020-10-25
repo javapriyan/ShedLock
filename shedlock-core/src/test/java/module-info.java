29780b245961ca90aed21263b8cf2560bb19b678
Lukas Krecan <lukas@krecan.net>
2020-10-25 10:25:04 +0100

    Introduce Java 9 modules

---
+++ shedlock-core/src/test/java/module-info.java
@@ -0,0 +1,9 @@
+open module net.javacrumbs.shedlock.core.test {
+    requires net.javacrumbs.shedlock.core;
+    requires org.slf4j;
+    requires org.jetbrains.annotations;
+
+    // Additional deps
+    requires org.junit.jupiter.api;
+    requires org.assertj.core;
+}
